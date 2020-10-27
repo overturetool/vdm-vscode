3e4799fb87f23ed7c1c8ab04bf006d50475e1c66
FrederikPM <frdrkpm@gmail.com>
2020-10-27 12:48:46 +0100

    Working on js to build the ct view

--- common/resources/ctView.js
+++ common/resources/ctView.js
@@ -2,19 +2,18 @@ const vscode = acquireVsCodeApi();
 
 let traces = [];
 
-function buildTable(cts, ctContainer)
+function buildTable(cts, table)
 {
-    //Access the DOM to get the table construct and add to it.
-    let table = document.createElement('table');
+    // Get the table construct and add to it.
     table.id = "ctTable";
     ctContainer.appendChild(table);
 
-    //Build the header row
+    // Build the header row
     let headers = ["Test case", "Result"]
     let thead = table.createTHead();
     let headerRow = thead.insertRow();
 
-    //Add the header row cells
+    // Add the header row cells
     for (let key of headers) {
         let th = document.createElement("th");       
         th.appendChild(document.createTextNode(key));
@@ -38,39 +37,62 @@ function buildTable(cts, ctContainer)
     }
 }
 
-function buildCTOutline(cts, ctContainer)
+function buildCTOutline(cts, container, resolve)
 {
-    let outerOL = document.createElement('ol');
-    outerOL.classList.add("outerOL");
+    let ctSymbolsList = document.createElement('ol');
+    ctSymbolsList.classList.add("outerOL");
     for(i = 0; i < cts.length; i++)
     {
-        // Add a list item to outerOL list
-        let li_ctSymbol = document.createElement('li');
-        outerOL.appendChild(li_ctSymbol);
+        // Add a list item to ctSymbolsList list
+        let ctSymbolsListItem = document.createElement('li');
+        ctSymbolsList.appendChild(ctSymbolsListItem);
 
-        // Add a details element to the list item
-        let details_ctSymbol = document.createElement('details');
-        li_ctSymbol.appendChild(details_ctSymbol);
-        details_ctSymbol.firstChild.textContent = cts[i].name;
+        // Add a details element to the list item - this element is the CTSymbol and contains the traces
+        let ctSymbolDetail = document.createElement('details');
+        ctSymbolsListItem.appendChild(ctSymbolDetail);
+        ctSymbolDetail.firstChild.textContent = cts[i].name;
 
         // Add a list to the details element 
-        let ul_ctSymbol = document.createElement('ul');
-        details_ctSymbol.appendChild(ul_ctSymbol);
-        let traces = cts[i].traces;
+        let tracesList = document.createElement('ul');
+        ctSymbolDetail.appendChild(tracesList);
 
-        // Add items to the list in the details element
+        // Add trace items to the list in the details element
+        let traces = cts[i].traces;
         for(l = 0; l < traces.length; l++)
         {
-            let ul_ctSymbol = document.createElement('ul');
-            details_ctSymbol.appendChild(ul_ctSymbol);
-            let traces[l]
-            for(n = 0; n < )
+            let tracesListItem = document.createElement('li');
+            testcaseList.appendChild(tracesListItem);
+
+            // Add a details element to the list item - this element is the trace and contains test cases for the trace
+            let tracesDetail = document.createElement('details');
+            tracesListItem.appendChild(tracesDetail);
+            tracesDetail.firstChild.textContent = traces[i].name;
+
+            // Add a list to the details element 
+            let testcaseList = document.createElement('ol');
+            tracesDetail.appendChild(testcaseList);
+
+            // Add test result items to the list in the details element if test cases are resolved
+            if(resolve)
+            {
+                let testResults = traces[l].testResults;
+                for(n = 0; n < testResults.legnth; n++)
+                {
+                    let testResultListItem = document.appendChild('li');
+
+                    // Add a details element to the list item - this element is the test result and contains the test case
+                    let testResultDetail = document.createElement('details');
+                    testResultListItem.appendChild(testResultDetail);
+                    testResultDetail.firstChild.textContent = testResults[i].id;
+
+                    // Add the test cases to the details element as a table
+                    let caseTable = document.createElement('table');
+                    testResultDetail.appendChild(caseTable);
+                    buildTable(testResults[l].cases, caseTable);
+                }
+            }
+            
         }
-
-
-
-
-
     }
-    outerOL.appendChild
+    ctSymbolsList.appendChild
 }
