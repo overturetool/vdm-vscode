1001340d7b3dd02acc49fc0fcd39651b0d405432
FrederikPM <frdrkpm@gmail.com>
2020-10-27 15:23:22 +0100

    Added interfaces to protocol for testing

--- common/src/protocol.lspx.ts
+++ common/src/protocol.lspx.ts
@@ -1,9 +1,18 @@
 import { Location } from "vscode";
 import { NotificationType, RequestType } from "vscode-languageclient";
 
+/**
+ * The experimental capabilities that the server can reply
+ */
+export interface ExperimentalCapabilities {
+    proofObligationProvider?: boolean;
+    combinatorialTestingProvider: boolean;
+}
 
-////////////////////////////////////////////// POG messsage extensions //////////////////////////////////////////////////
 
+/**
+ * Proof Obligation Generation interfaces and namespaces for extension messages
+ */
 export interface ProofObligation {
     id: number;
     name: string[];
@@ -17,12 +26,12 @@ export interface GeneratePOParams {
     uri: string;
 }
 
-export namespace GeneratePORequest {
-    export const type = new RequestType<GeneratePOParams, ProofObligation[] | null, void, void>('lspx/POG/generate');
-}
-
 export interface POGUpdatedParams {
     successful: boolean
+}
+
+export namespace GeneratePORequest {
+    export const type = new RequestType<GeneratePOParams, ProofObligation[] | null, void, void>('lspx/POG/generate');
 }
 
 export namespace POGUpdatedNotification {
@@ -30,8 +39,45 @@ export namespace POGUpdatedNotification {
 }
 
 /**
- * The experimental capabilities that the server can reply
+ * Combinatorial Testing interfaces and namespaces for extension messages
  */
-export interface ExperimentalCapabilities {
-    proofObligationProvider?: boolean
+
+export interface ctFilterOption {
+	key: string,                
+	value: string | number | boolean
+}
+
+
+export interface CTSymbol
+{
+	name: string,
+	traces: Trace[]
+}
+
+export interface Trace
+{
+	name: string,
+	id: number,
+	location: Location | null, 	
+	verdict: VerdictKind,
+	testResults?: TestResult[]
+}
+
+export interface TestResult
+{
+	id: number,
+	verdict: VerdictKind,
+	cases: TestCase[]
+}
+
+export enum VerdictKind {
+	Passed = 1,
+	Failed = 2,
+	Inconclusive = 3,
+	Filtered = 4,	
+}
+
+export interface TestCase {
+	case: string,
+	result: string | null
 }
