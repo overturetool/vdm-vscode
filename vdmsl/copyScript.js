300a3651d871a92b580f28acb28abb5e940b8722
FrederikPM <frdrkpm@gmail.com>
2020-11-02 13:16:04 +0100

    Moved copyScript to common and added icon

--- vdmsl/copyScript.js
+++
@@ -1,9 +0,0 @@
-var fs = require("fs-extra");
-var path = require('path');
-
-var scriptPath = process.cwd();
-
-fs.copy(path.join(scriptPath,'..','common','resources'), path.join(scriptPath, 'resources'), function (err) {
-  if (err) return console.error(err)
-    console.log('success!')
-});
