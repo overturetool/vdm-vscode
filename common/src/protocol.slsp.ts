87d70186ed5936bd5f12f8b23a5c18c1dae7545a
jonaskrask <jonas.krask@gmail.com>
2020-12-21 19:22:21 +0100

    Added Theorem Proving to protocol

--- common/src/protocol.slsp.ts
+++ common/src/protocol.slsp.ts
@@ -40,7 +40,8 @@ export interface TranslateParams extends WorkDoneProgressParams {
 	 */
 	languageId: string;
 	/**
-	 * DocumentUri specifying the location of the resulting translation.
+	 * DocumentUri specifying the location of the resulting 
+	 * translation.
 	 * This should be an existing empty folder.
 	 */
 	saveUri: DocumentUri;
@@ -58,7 +59,9 @@ export namespace TranslateRequest {
  */
 export interface TranslateResponse {
 	/**
-	 * URI specifying the "main" file of the resulting translation (if multiple files are generated, this is the uri to where "main" is).
+	 * URI specifying the "main" file of the resulting translation 
+	 * if multiple files are generated, this is the uri to where 
+	 * "main" is.
 	 */
 	uri: DocumentUri;
 }
@@ -327,3 +330,205 @@ export namespace CTExecuteRequest {
 	export const resultType = new ProgressType<CTTestCase[]>();
 }
 
+////////////////////// Theorem Proving (TP) ///////////////////////////////////
+/**
+ * Parameters for the lemmas request.
+ */
+interface TPLemmasParams {
+    /**
+     * The scope of the project files.
+     */
+    projectUri?: DocumentUri
+}
+
+/**
+ * TP/lemmas request and return type.
+ */
+export namespace TPLemmasRequest {
+	export const type = new RequestType<TPLemmasParams, Lemma[] | null, void, void>('slsp/TP/lemmas');
+}
+
+/**
+ * Parameters for the begin proof request.
+ */
+interface TPBeginProofParams {
+    /**
+     * Name of the lemma that is to be proved.
+     */
+    name: string
+}
+
+/**
+ * TP/beginProof request and return type.
+ */
+export namespace TPBeginProofRequest {
+	export const type = new RequestType<TPBeginProofParams, ProofState | null, void, void>('slsp/TP/beginProof');
+}
+
+/**
+ * Parameters for the prove request.
+ */
+interface TPProveParams {
+    /**
+     * Name of the lemma that is to be proved. 
+	 * If proof in progress that lemma is assumed.
+     */
+    name?: string
+}
+
+/**
+ * Response to the prove request.
+ */
+interface TPProveResponse {
+	/**
+	 * Status of the proof. 
+	 */
+	status: ProofStatus,
+	/**
+	 * Processing time in milliseconds
+	 */
+	time?: number,
+	/**
+	 * Suggested commands to apply
+	 */
+	command?: string[],
+	/**
+	 * Humans-readable description of:
+	 * Counter example, proof steps, etc.
+	 */
+    description?: string
+}
+
+
+/**
+ * TP/prove request and return type.
+ */
+export namespace TPProveRequest {
+	export const type = new RequestType<TPProveParams, TPProveResponse | null, void, void>('slsp/TP/prove');
+}
+
+/**
+ * TP/getCommands request and return type.
+ */
+export namespace TPGetCommandsRequest {
+	export const type = new RequestType<null, TPCommand[] | null, void, void>('slsp/TP/getCommands');
+}
+
+/**
+ * Parameters for the command request.
+ */
+interface TPCommandParams {
+    /**
+     * The command and arguments identified by a string.
+     */
+    command: string
+}
+
+/**
+ * Response to the command request.
+ */
+interface TPCommandResponse {
+	/**
+	 * Description of the result of the command, 
+	 * e.g. accepted, error, no change.
+	 */
+	description: string,
+	/**
+	 * State of the proof after the command.
+	 */
+    state: ProofState
+}
+
+/**
+ * TP/command request and return type.
+ */
+export namespace TPCommandRequest {
+	export const type = new RequestType<TPCommandParams, TPCommandResponse | null, void, void>('slsp/TP/command');
+}
+
+/**
+ * Parameters for the undo request.
+ */
+interface TPUndoParams {
+    /**
+	 * Id of the step that must be undone to.
+	 * If empty, undo last step.
+     */
+    id?: number
+}
+
+/**
+ * TP/undo request and return type.
+ */
+export namespace TPUndoRequest {
+	export const type = new RequestType<TPUndoParams, ProofState | null, void, void>('slsp/TP/undo');
+}
+
+/**
+ * Type describing the status of a proof
+ */
+type ProofStatus = "proved" | "disproved" | "untried" | "unfinished" | "timeout";
+
+/**
+ * Parameters describing a Lemma and meta data.
+ */
+interface Lemma
+{
+    /**
+	 * Unique name of the lemma.
+	 */
+	name: string,
+	/**
+	 * Name of the theory that the lemma belongs to.
+	 */
+	theory: string,
+	/**
+	 * Identifies the location of the lemma.
+	 */
+	location: Location,
+	/**
+	 * Theorem, Lemma, corollary etc.
+	 */
+	kind: string,
+	/**
+	 * Status of the proof of the lemma
+	 */
+    status: ProofStatus
+}
+
+/**
+ * Parameters describing the state of a proof and meta data.
+ */
+interface ProofState
+{
+	/**
+	 * Proof step id.
+	 */
+	id: number,
+	/**
+	 * Status of the proof. 
+	 */
+	status: ProofStatus | string,
+	/**
+	 * Subgoals, empty if proved.
+	 */
+	subgoals: string[],
+	/**
+	 * Rules used for this step.
+	 */
+    rules?: string[]
+}
+
+/**
+ * Parameters describing a theorem proving command.
+ */
+interface TPCommand{
+	/**
+	 * Command name.
+	 */
+	name: string,
+	/**
+	 * Description of the command.
+	 */
+    description: string
+}
