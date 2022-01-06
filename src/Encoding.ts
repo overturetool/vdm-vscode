ebeac076597fcb0a0110bd20630c70c1ac006247
Jonas Rask <jonas.krask@gmail.com>
2022-01-06 09:54:34 +0100

    #80 Don't show warning for empty files

--- src/Encoding.ts
+++ src/Encoding.ts
@@ -57,7 +57,7 @@ export function checkEncoding(document: TextDocument, logPath: string): void {
     }
 
     // Prompt user with warning, if not UTF-8
-    if (encodingDocument.encoding != 'ascii' && encodingDocument.encoding != 'UTF-8') {
+    if (encodingDocument.encoding != null && encodingDocument.encoding != 'ascii' && encodingDocument.encoding != 'UTF-8') {
         const encodingConfig = workspace.getConfiguration('vdm-vscode.encoding', wsFolder);
         if (encodingConfig?.showWarning) {
             window.showWarningMessage(`Document encoding is not UTF-8. Please set files.encoding to the correct encoding. Not doing so may cause issues for the VDM extensions`, 'Go to setting', 'Do not show again').then(
