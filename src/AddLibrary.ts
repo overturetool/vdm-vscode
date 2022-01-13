478aac5e48eacf9daef9441c66d38bee002d574f
FrederikPM <frdrkpm@gmail.com>
2022-01-13 15:55:52 +0100

    Implemented logic for extracting libraries from jar file(s).
    Still need to locate jar files in class path

--- src/AddLibrary.ts
+++ src/AddLibrary.ts
@@ -2,105 +2,173 @@
 
 import { commands, ExtensionContext, RelativePattern, Uri, window, workspace, WorkspaceFolder } from "vscode";
 import { SpecificationLanguageClient } from "./SpecificationLanguageClient";
-import * as path from 'path'
-import * as fs from 'fs-extra'
+import * as path from "path";
+import * as fs from "fs-extra";
+
+const yauzl = require("yauzl");
 
 export class AddLibraryHandler {
+	private readonly dialects = { vdmsl: "SL", vdmpp: "PP", vdmrt: "RT" };
 
-    constructor(
-        private readonly clients: Map<string, SpecificationLanguageClient>,
-        private context: ExtensionContext
-    ) {
-        commands.executeCommand('setContext', 'add-lib-show-button', true);
-        this.context = context;
-        this.registerCommand((inputUri: Uri) => this.addLibrary(workspace.getWorkspaceFolder(inputUri)));
-    }
+	constructor(private readonly clients: Map<string, SpecificationLanguageClient>, private context: ExtensionContext) {
+		commands.executeCommand("setContext", "add-lib-show-button", true);
+		this.context = context;
+		this.registerCommand((inputUri: Uri) => this.addLibrary(workspace.getWorkspaceFolder(inputUri)));
+	}
 
-    private registerCommand = (callback: (...args: any[]) => any) => {
-        let disposable = commands.registerCommand("vdm-vscode.addLibrary", callback)
-        this.context.subscriptions.push(disposable);
-        return disposable;
-    };
+	private registerCommand = (callback: (...args: any[]) => any) => {
+		let disposable = commands.registerCommand("vdm-vscode.addLibrary", callback);
+		this.context.subscriptions.push(disposable);
+		return disposable;
+	};
 
-    private async addLibrary(wsFolder: WorkspaceFolder) {
+	private async addLibrary(wsFolder: WorkspaceFolder) {
+		window.setStatusBarMessage(
+			`Adding Libraries.`,
+			new Promise(async (resolve, reject) => {
+				this.getDialect(wsFolder).then((dialect) => {
+					// Gather available libraries and let user select
+					const jarsPath = path.resolve(this.context.extensionPath, "resources", "jars", "vdmj");
 
-        let dialect = null;
-        const dialects = { "vdmsl": "SL", "vdmpp": "PP", "vdmrt": "RT" }
+					Promise.all(fs.readdirSync(jarsPath, { withFileTypes: true }).map((entry) => this.extractLibrariesFromJar(path.join(jarsPath, entry.name), dialect))).then(
+						async (libsStreamMaps) => {
+							const reducedStreamMap = libsStreamMaps.reduce((prev, curr) => new Map([...Array.from(prev.entries()), ...Array.from(curr.entries())]));
 
-        window.setStatusBarMessage(`Adding Libraries.`, new Promise(async (resolve, reject) => {
-            let client = this.clients.get(wsFolder.uri.toString());
-            if (client) {
-                dialect = dialects[client.language];
-            } else {
-                console.log(`No client found for the folder: ${wsFolder.name}`);
+							const selectedLibs: string[] = await window.showQuickPick(Array.from(reducedStreamMap.keys()), {
+								placeHolder: "Choose libraries",
+								canPickMany: true,
+							});
 
-                // Guess dialect
-                for (var dp in dialects) {
-                    let pattern = new RelativePattern(wsFolder.uri.path, "*." + dp);
-                    let res = await workspace.findFiles(pattern, null, 1)
-                    if (res.length == 1) dialect = dialects[dp];
-                }
+							// None selected
+							if (selectedLibs === undefined || selectedLibs.length == 0) return resolve(`Empty selection. Add library completed.`);
 
-                if (dialect == null) {
-                    // TODO could insert a selection window here so that the user can manually choose the dialect if we can't guess
-                    window.showInformationMessage(`Add library failed! Unable to guess VDM dialect for workspace`);
-                    reject();
-                    return;
-                }
-            }
+							const folderPath = path.resolve(wsFolder.uri.fsPath, "lib");
 
-            // Gather available libraries and let user select
-            const libPath = path.resolve(this.context.extensionPath, "resources", "lib", dialect);
+							fs.ensureDir(folderPath)
+								.then(async () => {
+									try {
+										const wsEncoding = workspace.getConfiguration("files", wsFolder).get("encoding", "utf8");
+										if (!Buffer.isEncoding(wsEncoding)) console.log(`Encoding (files.encoding: ${wsEncoding}) not possible using the default: UTF-8`);
 
-            const libsInFolder: fs.Dirent[] = fs.readdirSync(libPath, { withFileTypes: true });
+										selectedLibs.forEach((libName) => {
+											const writeStream = fs.createWriteStream(path.join(folderPath, libName), { flags: "a" });
+											reducedStreamMap
+												.get(libName)
+												.pipe(writeStream)
+												.on("error", (err) => {
+													window.showInformationMessage(`Add library ${libName} failed`);
+													console.log(`Copy library files failed with error: ${err}`);
+												});
+										});
+										resolve(`Add library completed.`);
+									} catch (error) {
+										window.showWarningMessage(`Add library failed with error: ${error}`);
+										console.log(`Add library failed with error: ${error}`);
+										reject(`Add library failed with error: ${error}`);
+									}
+								})
+								.catch((error) => {
+									window.showWarningMessage("Creating directory for library failed");
+									console.log(`Creating directory for library files failed with error: ${error}`);
+									reject(`Creating directory for library files failed with error: ${error}`);
+								});
+						}
+					);
+				});
 
-            let libsOptions: string[] = libsInFolder.map((x: fs.Dirent) => x.name);
+				// const folderPath = path.resolve(wsFolder.uri.fsPath, "lib");
+				// fs.ensureDir(folderPath)
+				// 	.then(async () => {
+				// 		try {
+				// 			const wsEncoding = workspace.getConfiguration("files", wsFolder).get("encoding", "utf8");
+				// 			if (!Buffer.isEncoding(wsEncoding)) console.log(`Encoding (files.encoding: ${wsEncoding}) not possible using the default: UTF-8`);
 
-            let selectedLibs: string[] = await window.showQuickPick(libsOptions, {
-                placeHolder: 'Choose libraries',
-                canPickMany: true,
-            });
+				// 			for (const lib of selectedLibs) {
+				// 				const src = path.resolve(libPath, lib);
+				// 				const dest = path.resolve(folderPath, lib);
 
-            // None selected 
-            if (selectedLibs === undefined || selectedLibs.length == 0) return resolve(`Empty selection. Add library completed.`)
+				// 				// Copy files to project with UTF-8 encoding
+				// 				if (wsEncoding == "utf8" || !Buffer.isEncoding(wsEncoding)) {
+				// 					// Copy library from resources/lib to here
+				// 					fs.copyFile(src, dest).catch((e) => {
+				// 						window.showInformationMessage(`Add library ${lib} failed`);
+				// 						console.log(`Copy library files failed with error: ${e}`);
+				// 					});
+				// 				} else {
+				// 					// Convert encoding
+				// 					fs.writeFileSync(dest, fs.readFileSync(src, { encoding: "utf8" }), { encoding: wsEncoding });
+				// 				}
+				// 			}
+				// 			resolve(`Add library completed.`);
+				// 		} catch (error) {
+				// 			window.showWarningMessage(`Add library failed with error: ${error}`);
+				// 			console.log(`Add library failed with error: ${error}`);
+				// 			reject();
+				// 		}
+				// 	})
+				// 	.catch((error) => {
+				// 		window.showWarningMessage("Creating directory for library failed");
+				// 		console.log(`Creating directory for library files failed with error: ${error}`);
+				// 		reject();
+				// 	});
+			})
+		);
+	}
 
-            const folderPath = path.resolve(wsFolder.uri.fsPath, "lib");
-            fs.ensureDir(folderPath).then(async () => {
-                try {
-                    const wsEncoding = workspace.getConfiguration('files', wsFolder).get('encoding', 'utf8');
-                    if (!Buffer.isEncoding(wsEncoding))
-                        console.log(`Encoding (files.encoding: ${wsEncoding}) not possible using the default: UTF-8`)
+	private getDialect(wsFolder: WorkspaceFolder): Promise<string> {
+		return new Promise<string>(async (resolve, reject) => {
+			const client = this.clients.get(wsFolder.uri.toString());
+			if (client) {
+				resolve(this.dialects[client.language]);
+			} else {
+				console.log(`No client found for the folder: ${wsFolder.name}`);
 
-                    for (const lib of selectedLibs) {
-                        const src = path.resolve(libPath, lib);
-                        const dest = path.resolve(folderPath, lib);
+				// Guess dialect
+				for (const dialect in this.dialects) {
+					const pattern = new RelativePattern(wsFolder.uri.path, "*." + dialect);
+					if ((await workspace.findFiles(pattern, null, 1)).length == 1) resolve(this.dialects[dialect]);
+				}
 
-                        // Copy files to project with UTF-8 encoding
-                        if (wsEncoding == 'utf8' || !Buffer.isEncoding(wsEncoding)) {
-                            // Copy library from resources/lib to here
-                            fs.copyFile(src, dest).catch((e) => {
-                                window.showInformationMessage(`Add library ${lib} failed`);
-                                console.log(`Copy library files failed with error: ${e}`);
-                            });
-                        }
-                        else {
-                            // Convert encoding
-                            fs.writeFileSync(dest, fs.readFileSync(src, { encoding: 'utf8' }), { encoding: wsEncoding });
-                        }
-                    }
-                    resolve(`Add library completed.`);
-                }
-                catch (error) {
-                    window.showWarningMessage(`Add library failed with error: ${error}`);
-                    console.log(`Add library failed with error: ${error}`);
-                    reject();
-                }
-            }).catch(error => {
-                window.showWarningMessage("Creating directory for library failed");
-                console.log(`Creating directory for library files failed with error: ${error}`);
-                reject();
-            });
-        }));
+				const dialect: string = await window.showQuickPick(Object.keys(this.dialects), {
+					placeHolder: "Choose dialect",
+					canPickMany: false,
+				});
+				if (!dialect) {
+					window.showInformationMessage(`Add library failed! Unable to determine VDM dialect for workspace`);
+					reject();
+				}
 
-    }
+				resolve(this.dialects[dialect]);
+			}
+		});
+	}
+
+	private extractLibrariesFromJar(jarsPath: string, dialect: string): Promise<Map<string, fs.ReadStream>> {
+		return new Promise<Map<string, fs.ReadStream>>(async (resolve, reject) => {
+			try {
+				const fileNamesToStreams = new Map<string, fs.ReadStream>();
+				yauzl.open(jarsPath, { lazyEntries: true, autoClose: true }, (err, zipfile) => {
+					if (err) reject(err);
+					zipfile.on("end", () => {
+						resolve(fileNamesToStreams);
+					});
+					zipfile.on("error", (err) => {
+						reject(err);
+					});
+					zipfile.on("entry", (entry) => {
+						if (!/\/$/.test(entry.fileName) && entry.fileName.toUpperCase().endsWith(dialect)) {
+							zipfile.openReadStream(entry, (error, readStream) => {
+								if (error) reject(error);
+								fileNamesToStreams.set(path.basename(entry.fileName), readStream);
+							});
+						}
+						zipfile.readEntry();
+					});
+					zipfile.readEntry();
+				});
+			} catch (exception) {
+				reject(exception);
+			}
+		});
+	}
 }
