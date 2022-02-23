e7e272cc72c1045305e20d2e2b593f2025353a63
Jonas Rask <jonas.krask@gmail.com>
2022-02-23 12:42:58 +0100

    Added dialect util function and tests
    The tests are using mocha, but I can't get the to run properly..

--- test/index.ts
+++ test/index.ts
@@ -2,42 +2,60 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
-import * as path from 'path';
-import * as Mocha from 'mocha';
-import * as glob from 'glob';
+import * as path from "path";
+import * as Mocha from "mocha";
+import * as glob from "glob";
 
-export function run(): Promise<void> {
-	// Create the mocha test
-	const mocha = new Mocha({
-		ui: 'tdd',
-		color: true
-	});
-	mocha.timeout(100000);
+export async function run(): Promise<void> {
+    // Create the mocha test
+    const mocha = new Mocha({
+        ui: "tdd",
+        color: true,
+    });
+    const testsRoot = path.join(__dirname);
 
-	const testsRoot = __dirname;
+    // Enable source map support
+    // require('source-map-support').install();
 
-	return new Promise((resolve, reject) => {
-		glob('**.test.js', { cwd: testsRoot }, (err, files) => {
-			if (err) {
-				return reject(err);
-			}
+    // Find test files
+    const testFiles = await new Promise<string[]>((resolve, reject) => {
+        glob(`**/**.test.js`, { ignore: ["**/**.test.js", "**/**.functional.test.js"], cwd: testsRoot }, (error, files) => {
+            if (error) {
+                return reject(error);
+            }
+            resolve(files);
+        });
+    });
 
-			// Add files to the test suite
-			files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+    // Setup test files that need to be run
+    testFiles.forEach((file) => mocha.addFile(path.join(testsRoot, file)));
 
-			try {
-				// Run the mocha test
-				mocha.run(failures => {
-					if (failures > 0) {
-						reject(new Error(`${failures} tests failed.`));
-					} else {
-						resolve();
-					}
-				});
-			} catch (err) {
-				console.error(err);
-				reject(err);
-			}
-		});
-	});
+    // TODO Needed?
+    // // // Activate extension
+    // // console.time('Time taken to activate the extension');
+    // // try {
+    // //     await activatePythonExtensionScript(); // see https://github.com/microsoft/vscode-python/blob/main/src/test/index.ts
+    // //     console.timeEnd('Time taken to activate the extension');
+    // // } catch (ex) {
+    // //     console.error('Failed to activate python extension without errors', ex);
+    // // }
+
+    // Run the tests
+    try {
+        await new Promise<void>((resolve, reject) => {
+            mocha
+                .run((failures) => {
+                    if (failures > 0) {
+                        return reject(new Error(`${failures} total failures`));
+                    }
+                    resolve();
+                })
+                .uncaught((err: any) => {
+                    console.error(err);
+                    reject(err);
+                });
+        });
+    } catch (e) {
+        console.error(e);
+    }
 }
---
+++ test/util/Dialect.unit.test.ts
@@ -0,0 +1,68 @@
+import { expect } from "chai";
+import { getDialectFromAlias, getDialectFromPretty, isVDMFile } from "../../src/util/Dialect";
+
+describe("getDialectFromAlias", () => {
+    it("throw error on mismatch", () => {
+        expect(() => getDialectFromAlias("badAlias")).to.throw(Error);
+    });
+
+    [
+        ["vdmsl", "vdmsl"],
+        ["vsl", "vdmsl"],
+        ["vdm-sl", "vdmsl"],
+        ["sl", "vdmsl"],
+        ["vdmpp", "vdmpp"],
+        ["vpp", "vdmpp"],
+        ["vdm-pp", "vdmpp"],
+        ["pp", "vdmpp"],
+        ["vdm++", "vdmpp"],
+        ["vdmrt", "vdmrt"],
+        ["vrt", "vdmrt"],
+        ["vdm-rt", "vdmrt"],
+        ["rt", "vdmrt"],
+    ].forEach((testInput) => {
+        it("return valid dialects on direct match", () => {
+            expect(getDialectFromAlias(testInput[0])).to.be(testInput[1]);
+        });
+    });
+
+    [
+        ["VDMSL", "vdmsl"],
+        ["VdM++", "vdmpp"],
+        ["VDM-rt", "vdmrt"],
+    ].forEach((testInput) => {
+        it("return valid dialects on large letters", () => {
+            expect(getDialectFromAlias(testInput[0])).to.be(testInput[1]);
+        });
+    });
+});
+
+describe("getDialectFromPretty", () => {
+    [
+        ["VDM-SL", "vdmsl"],
+        ["VDM++", "vdmpp"],
+        ["VDM-RT", "vdmrt"],
+    ].forEach((testInput) => {
+        it("return dialects on match", () => {
+            expect(getDialectFromPretty(testInput[0])).to.be(testInput[1]);
+        });
+    });
+
+    it("return undefined on no match", () => {
+        expect(getDialectFromPretty("badInput")).to.be.undefined;
+    });
+});
+
+describe("isVDMFile", () => {
+    it("return true for vdm file extension", () => {
+        expect(isVDMFile("some.path.vdmsl")).to.be.true;
+    });
+
+    it("return false for non vdm file extension", () => {
+        expect(isVDMFile("some.invalid.vdmsl.path")).to.be.false;
+    });
+});
+
+// describe("pickDialect", () => {
+//     test("");
+// });
