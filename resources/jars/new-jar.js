0bcb4bec72079b1164496a839a7f87b8a2078dd1
Laursen <201703930@post.au.dk>
2023-03-10 15:31:14 +0100

    Glenn & Saeed MSc: Testing new-jar js.

--- resources/jars/new-jar.js
+++ resources/jars/new-jar.js
@@ -3,22 +3,23 @@ const execSync = require('child_process').execSync;
 
 var args = process.argv.slice(2);
 
-if (args.length < 1) throw new Error("repository argument required");
+if (args.length < 1) {
+    throw new Error("repository argument required");
+}
 
 let repo = args.find((a) => a.includes("repo=")).slice("repo=".length)
 let groupId = args.find((a) => a.includes("groupId=")).slice("groupId=".length)
 let artifactId = args.find((a) => a.includes("artifactId=")).slice("artifactId=".length)
 let version = args.find((a) => a.includes("version=")).slice("version=".length)
 
-let JarInfo = data.jars.find((j) => j.repository === repo)
+let jarInfo = data.jars.find((j) => j.repository === repo)
 
-if (!JarInfo) throw new Error("Repository is not allowed");
+if (!jarInfo) {
+    throw new Error("Repository is not allowed");
+}
 
-const output = execSync(`mvn dependency:copy \
+const output = execSync(`mvn dependency:get \
     -Dartifact=${groupId}:${artifactId}:${version} \
-    -DoutputDirectory=${JarInfo.dest_dir} \
-    -Dmdep.useBaseVersion=true \
-    -Dmdep.overIfNewer=true \
-    -Dmdep.overWriteSnapshots=true \
-    -Dmdep.overWriteReleases=true`, { encoding: 'utf-8' })
+    -DremoteRepositories=github::default::https://maven.pkg.github.com/${jarInfo.repository} \
+    -Ddest=${jarInfo.dest_dir}/${artifactId}-${version}.jar`, {encoding: 'utf-8'})
 console.log(output);
